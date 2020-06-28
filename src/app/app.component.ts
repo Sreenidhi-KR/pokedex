1b7360e39de147dbf2dd5fdedc9af37c504ef05f
unknown <rsri1998@gmail.com>
2020-06-28 12:08:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pokemon-app';
+  title = 'pokemon';
+  pokemons = [];
+  initpokemons = [];
+  categories = [];
+  checkoutForm: FormGroup;
+  loading = true;
+  constructor(apollo: Apollo, private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      catagory: '',
+      name: '',
+    });
+    apollo
+      .query({
+        query: gql`
+          {
+            query {
+              pokemons(first: 550) {
+                name
+                types
+                classification
+                image
+              }
+            }
+          }
+        `,
+      })
+      .subscribe(({ data, loading }: { data: any; loading: boolean }) => {
+        this.initpokemons = data.query.pokemons;
+        this.pokemons = this.initpokemons;
+        this.loading = loading;
+        this.categories = this.initpokemons
+          .map((c) => c.types)
+          .reduce((acc, val) => acc.concat(val), []);
+        this.categories = [...new Set(this.categories)];
+      });
+  }
+  onSubmit(cat) {
+    this.pokemons = this.initpokemons;
+    const filteredPokemon = this.pokemons.filter((poke) => {
+      return poke.types.includes(cat.catagory);
+    });
+    this.pokemons = filteredPokemon;
+  }
+  onChange(event) {
+    this.pokemons = this.initpokemons;
+    const searchedPokemons = this.pokemons.filter((poke) => {
+      return poke.name.toLowerCase().includes(event.toLowerCase());
+    });
+    this.pokemons = searchedPokemons;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ApolloModule,
+    ReactiveFormsModule,
+    HttpLinkModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'https://graphql-pokemon.now.sh/' }),
+      cache: new InMemoryCache(),
+    });
+  }
+}
